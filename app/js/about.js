f93222ad6dcf6c59f624164416dfb1a827239e71
loveky <ylzcylx@gmail.com>
2015-09-26 18:49:53 +0800

    save

--- app/js/about.js
+++ app/js/about.js
@@ -1,10 +1,3 @@
 var remote = require('remote');
 var aboutWindow = remote.getCurrentWindow();
 aboutWindow.setMenu(null);
-
-aboutWindow.focus();
-
-aboutWindow.on('blur', function () {
-	aboutWindow.focus();
-	return false;
-});
